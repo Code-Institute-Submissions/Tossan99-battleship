a907de8aafd7070e72e7a26a4619a27deee8eea5
Tossan99 <>
2023-09-07 18:21:18 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+import random
+
+
+def main():
+    """
+    Calls all functions
+    """
+
+    print("Welcome to Battleships")
+    username = input("Enter your username: \n")
+    print()
+
+
+main()
