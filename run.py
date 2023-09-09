10a3e312bf5036b2f8a8fc8cf10d367a39b950ed
Tossan99 <>
2023-09-09 14:56:32 +0000

    Add print statments to create_board function

--- run.py
+++ run.py
@@ -55,8 +55,11 @@ class Battlefield:
 
     def create_board(self):
         print(self.name)
+        print("  0 1 2 3 4 5 6 7")
+        i = 0
         for x in self.board:
-            print(*x)
+            print(i, *x)
+            i += 1
         print("-"*30)
 
 
